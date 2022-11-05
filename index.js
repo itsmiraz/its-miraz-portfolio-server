41e7992661ddbe1b7a8b70ad064aea38a443169b
itsmiraz <mirajulislamofficial044@gmail.com>
2022-11-05 10:39:39 +0600

    dfas

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
